8958314cad86342de2da828e0ad3fce15e52c402
Mohib Ali <mohibalikhan16@gmail.com>
2025-02-23 05:39:46 +0500

    Updated Files

---
+++ django_server.cmd
@@ -0,0 +1,8 @@
+@REM This cmd for Directory handling
+
+cd backend
+
+
+@REM This cmd for start Django server
+
+py manage.py runserver
---
+++ react_server.cmd
@@ -0,0 +1,6 @@
+@REM This cmd for Directory handling
+cd frontend\todolist
+
+@REM This cmd for server start
+
+npm start
